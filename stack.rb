edbeb378d01c9a4eb7a6d0a7f8cf113b1eb9bce6
Omar Salem <omarsalem0721@gmail.com>
2022-10-18 15:14:30 +0200

    Add task solution for stack

---
+++ stack.rb
@@ -0,0 +1,46 @@
+require_relative './node.rb'
+
+class Stack
+    def initialize
+        @first = nil
+        @size = 0
+        @sum = 0
+    end
+
+    def push(number)
+        @sum =@sum+number
+        @size=@size+1
+        @first = Node.new(number, @first)
+        @max = @first if @max.nil?
+        @max = Node.new(number, @max) if number >= self.max
+    end
+
+    def pop
+        @size=@size-1
+        @max = @max.next_node if @max.value == @first.value
+        item = @first
+        @sum =@sum- item.value
+        @first = @first.next_node
+        item.value
+    end
+    
+    def mean
+        @sum.to_f/@size
+    end
+
+    def max
+        @max.value if !@max.nil?
+    end
+
+end
+
+
+stack = Stack.new
+stack.push(2)
+stack.push(4)
+stack.push(6)
+stack.push(12)
+stack.pop
+
+p stack.max
+p stack.mean
